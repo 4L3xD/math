9cf830438abae34708c7f5e370cc5d5b80bfed17
4L3_xD <alessandra.olea@gmail.com>
2020-05-16 23:08:22 -0300

    soma de fraçoes concluida

--- fracao.py
+++ fracao.py
@@ -1,17 +1,18 @@
 numeradorF1 = int(input('Digite o numerador da primeira fração: '))
 denominadorF1 = int(input('Digite o denominador da primeira fração: '))
-print('A fração é {}/{}.'.format(numeradorF1, denominadorF1))
+print('A primeira fração é {}/{}.'.format(numeradorF1, denominadorF1))
 
 numero = int(input('Digite um inteiro: '))
 fracaoMista = int(denominadorF1*numero+numeradorF1)
 
 print('Soma de fração mista: {}/{} + {} = ({}*{}+{})/{} = {}/{} = (denominador fração * numero inteiro + numerador fração) / denominador fração'
 .format(numeradorF1, denominadorF1, numero, denominadorF1, numero, numeradorF1, denominadorF1, fracaoMista, denominadorF1))
+print('Logo, {}/{} = {}'.format(fracaoMista, denominadorF1, fracaoMista/denominadorF1))
 
 numeradorF2 = int(input('Digite o numerador da segunda fração: '))
 denominadorF2 = int(input('Digite o denominador da segunda fração: '))
-print('A fração é {}/{}.'.format(numeradorF2, denominadorF2))
+print('A segunda fração é {}/{}.'.format(numeradorF2, denominadorF2))
 
-mmc = ''
+soma = (numeradorF1 * denominadorF2 + numeradorF2 * denominadorF1) / (denominadorF1 * denominadorF2)
 
-print('Soma de frações: {}/{} + {}/{} = '.format(numeradorF1, denominadorF1, numeradorF2, denominadorF2))
+print('Soma de frações: {}/{} + {}/{} = {}'.format(numeradorF1, denominadorF1, numeradorF2, denominadorF2, soma))
