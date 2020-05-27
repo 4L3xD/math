35e6f95ffab49b91313e89d29a6ab3b0bac2e8e5
4L3_xD <alessandra.olea@gmail.com>
2020-05-27 12:53:30 -0300

    add P6 and P7

--- exponenciacao.py
+++ exponenciacao.py
@@ -1,11 +1,7 @@
 # TO DO:
-# a^b*a^c = a^(b+c)
-# a^b/a^c = a^(b-c)
-# a^b^c = a^(a*c)
 # a^c*b^c = (a^c)*(b^c)
 # (a/b)^c = a^c/b^c
 # (a/b)^-c = (b/a)^c
-
 
 print('Propriedades Exponenciação/Potenciação\n\nP1: a¹ = a, P2: a⁰ = 1\n')
 a = int(input('Digite a: '))
@@ -32,5 +28,20 @@ c = int(input('Digite c: '))
 print(c)
 print('{}**{}/{} = {}^{}'.format(a, b, c, a, b/c))
 print('O índice (c) na radiciação é o denominador do expoente fracionário na exponenciação em R* \nA base (a) na radiciação é a base da exponenciação.\nO expoente (b) da radiciação é o numerador da exponenciação fracionária dividido pelo índice da radiciação (b/c). \nOu seja, quando se eleva um número Z por uma fração obtém-se a raíz do número inteiro.')
-# print('a**(b/c) = {:.3f}'.format(a**(b/c))); output: 1.260;
+# print('a**(b/c) = {:.3f}'.format(a**(b/c))); output: 1.260 para imput a=2, b=1, c=3;
 print('a**(b/c) = {}'.format(a**(b/c))) # output: 1.2599210498948732
+
+# a^b*a^c = a^(b+c)
+print('\nP5: (a**b)*(a**c) = a**(b+c)')
+print('({}**{})*({}**{}) = {} = '.format(a, b, a, c, a**b*a**c))
+print('{}**({}+{}) = {} = '.format(a, b, c, a**(b+c)))
+
+# a^b/a^c = a^(b-c)
+print('\nP6: a**b/a**c = a**(b-c)')
+print('{}**{}/{}**{} = {}'.format(a, b, a, c, a**b/a**c))
+print('{}**({}-{}) = {}'.format(a, b, c, a**(b-c)))
+
+# a^b^c = a^(a*c)
+print('\nP7: (a**b)**c = a**(b*c)')
+print('({}**{})**{} = {}'.format(a, b, c, (a**b)**c))
+print('{}**({}*{}) = {}'.format(a, b, c, a**(b*c)))
